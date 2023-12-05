3b5c1fd45987de65166ea77caf33ba9800871458
iluxaBoy <illapetefi@gmail.com>
2023-12-05 15:56:44 +0200

    T-1 add noise

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import Loader from "./components/loader/Loader.vue"
-// import Grid from "./components/HomeAnimation/Grid.vue";
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="noise"><img src="src\assets\img\шум.png" alt=""></div>
   <!-- <Loader /> -->
   <!-- <Grid /> -->
   <RouterView />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.noise {
+  position: fixed;
+  top: 0;
+  left: 0;
+  opacity: 0.06;
+}
+</style>
